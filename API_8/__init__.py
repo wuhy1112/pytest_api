458366f4d69b449ab663fa68cc8a7ad55d261ce7
Your Name <306210334@qq.com>
2019-04-29 11:45:22 +0800

    new add

---
+++ API_8/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/29 11:36
+# @Author  : wuhy
---
+++ API_8/run.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 _*-
+""" 
+@author:mongo
+@time: 2018/12/17 
+@email:3126972006@qq.com
+@function： 
+"""
+import sys
+import unittest
+
+sys.path.append("./")
+
+
+from API_8.common import HTMLTestRunnerNew
+from API_8.common import contants
+
+
+
+discover = unittest.defaultTestLoader.discover(contants.case_dir, "test_*.py")
+
+with open(contants.report_dir + '/report.html', 'wb+') as file:
+    runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file,
+                                              title="PYTHON15 API TEST REPORT",
+                                              description="q前程贷API",
+                                              tester="mongo")
+    runner.run(discover)
