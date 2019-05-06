9d270a25a9f96e1c1030fedf67eb83aa5f108e3c
Your Name <306210334@qq.com>
2019-05-06 13:15:34 +0800

    new add

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/6 13:14
+# @Author  : wuhy
---
+++ test/test_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/6 13:14
+# @Author  : wuhy
+
+
+print(1+2)
